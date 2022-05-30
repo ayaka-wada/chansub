181fe67e7e17fd5aa74376de15e40085b6f5e1d7
ayaka-wada <s2022040@stu.musashino-u.ac.jp>
2022-05-31 04:05:25 +0900

    chansub.py 追記

--- src/chansub.py
+++ src/chansub.py
@@ -13,46 +13,46 @@ sheet=wb['Sheet1']
 wb.save('result.xlsx')
 d=pd.read_excel('result.xlsx',engine='openpyxl',sheet_name='Sheet1')
 size=0
-countries=[]
-for i in d.Country:
- if i!='兎田ぺこら':
-  countries.append(i)
+names=[]
+for i in d.Name:
+ if i!='Usada Pekora':
+  names.append(i)
   size=size+1
  else: 
-  countries.append(i)
+  names.append(i)
   size=size+1
   break
-print(len(countries),': ',countries)
+print(len(names),': ',names)
 no=len(sys.argv)-1
 x=[]
 for i in range(2000,2021):
  x.append(i)
 cnt=[]
 for i in range(no):
- if sys.argv[i+1] in countries:
+ if sys.argv[i+1] in names:
   #print(sys.argv[i+1])
-  cnt.append(d.loc[d.Country==sys.argv[i+1]])
+  cnt.append(d.loc[d.Name==sys.argv[i+1]])
  else: 
   print('correct the name of ',sys.argv[i+1])
-cntry=[]
+nme=[]
 #print(len(cnt))
 for j in range(len(cnt)):
  for i in range(2000,2021):
-  cntry.append(int(cnt[j][i]))
+  nme.append(int(cnt[j][i]))
 if len(cnt)==1:
- plt.plot(x,cntry,'k-',label=sys.argv[1])
+ plt.plot(x,nme,'k-',label=sys.argv[1])
 if len(cnt)==2:
- plt.plot(x,cntry[0:21],'k-',label=sys.argv[1])
- plt.plot(x,cntry[21:42],'k--',label=sys.argv[2])
+ plt.plot(x,nme[0:21],'k-',label=sys.argv[1])
+ plt.plot(x,nme[21:42],'k--',label=sys.argv[2])
 if len(cnt)==3:
- plt.plot(x,cntry[0:21],'k-',label=sys.argv[1])
- plt.plot(x,cntry[21:42],'k--',label=sys.argv[2])
- plt.plot(x,cntry[42:63],'k:',label=sys.argv[3])
+ plt.plot(x,nme[0:21],'k-',label=sys.argv[1])
+ plt.plot(x,nme[21:42],'k--',label=sys.argv[2])
+ plt.plot(x,nme[42:63],'k:',label=sys.argv[3])
 if len(cnt)==4:
- plt.plot(x,cntry[0:21],'k-',label=sys.argv[1])
- plt.plot(x,cntry[21:42],'k--',label=sys.argv[2])
- plt.plot(x,cntry[42:63],'k:',label=sys.argv[3])
- plt.plot(x,cntry[63:84],'k-.',label=sys.argv[4])
+ plt.plot(x,nme[0:21],'k-',label=sys.argv[1])
+ plt.plot(x,nme[21:42],'k--',label=sys.argv[2])
+ plt.plot(x,nme[42:63],'k:',label=sys.argv[3])
+ plt.plot(x,nme[63:84],'k-.',label=sys.argv[4])
 def main():
  plt.legend()
  plt.savefig('result.png')
