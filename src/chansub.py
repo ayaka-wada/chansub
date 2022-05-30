6adbe6d760c005a852e0e988a5c993d7442a6beb
ayaka-wada <s2022040@stu.musashino-u.ac.jp>
2022-05-31 05:01:55 +0900

    編集

--- src/chansub.py
+++ src/chansub.py
@@ -1,20 +1,24 @@
 import subprocess as sp
+
+import pandas
 import pandas as pd
 import matplotlib.pyplot as plt
 import openpyxl
 import sys, os
-if os.path.exists('./SIPRI-Milex-data-1949-2020_0.xlsx'):
- wb = openpyxl.load_workbook('SIPRI-Milex-data-1949-2020_0.xlsx')
+if os.path.exists('./Number_of_channel_subscribers.xlsx'):
+ wb = openpyxl.load_workbook('Number_of_channel_subscribers.xlsx')
 else:
  sp.call("wget https://github.com/ayaka-wada/chansub/blob/main/src/Number_of_channel_subscribers.xlsx", shell=True)
  wb = openpyxl.load_workbook('Number_of_channel_subscribers.xls')
-sheet = wb['Sheet1']
+ # wb = pandas.read_excel('Number_of_channel_subscribers.xlsx', engine = 'openpyxl')
+# sheet = wb['Sheet1']
 # sheet.delete_rows(sheet.min_row,5)
 wb.save('result.xlsx')
-d = pd.read_excel('result.xlsx', engine='openpyxl', sheet_name='Sheet1')
+d = pd.read_excel('result.xlsx', engine='openpyxl')
 size = 0
 names = []
-for i in d.Name:
+
+for i in d.name:
  if i != 'Usada Pekora':
   names.append(i)
   size=size+1
@@ -31,7 +35,7 @@ cnt = []
 for i in range(no):
  if sys.argv[i+1] in names:
   #print(sys.argv[i+1])
-  cnt.append(d.loc[d.Name==sys.argv[i+1]])
+  cnt.append(d.loc[d.name==sys.argv[i+1]])
  else:
   print('correct the name of ', sys.argv[i+1])
 
