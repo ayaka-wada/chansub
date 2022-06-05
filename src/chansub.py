a0bd1b661004b5ee934cf4f65f04aeb401f63e1a
ayaka-wada <s2022040@stu.musashino-u.ac.jp>
2022-06-06 02:24:07 +0900

    ファイル名変更

--- src/chansub.py
+++ src/chansub.py
@@ -1,69 +1,36 @@
-import subprocess as sp
-
-import pandas
 import pandas as pd
 import matplotlib.pyplot as plt
-import openpyxl
-import sys, os
-if os.path.exists('./Number_of_channel_subscribers.xlsx'):
- wb = openpyxl.load_workbook('Number_of_channel_subscribers.xlsx')
-else:
- sp.call("wget https://github.com/ayaka-wada/chansub/blob/main/src/Number_of_channel_subscribers.xlsx", shell=True)
- wb = openpyxl.load_workbook('Number_of_channel_subscribers.xls')
- # wb = pandas.read_excel('Number_of_channel_subscribers.xlsx', engine = 'openpyxl')
-# sheet = wb['Sheet1']
-# sheet.delete_rows(sheet.min_row,5)
-wb.save('result.xlsx')
-d = pd.read_excel('result.xlsx', engine='openpyxl')
-size = 0
-names = []
+import urllib.request
+from io import StringIO
 
-for i in d.name:
- if i != 'Usada Pekora':
-  names.append(i)
-  size=size+1
- else: 
-  names.append(i)
-  size=size+1
-  break
-print(len(names), ': ', names)
-no = len(sys.argv)-1
-x = []
-for i in range(2000, 2022):
- x.append(i)
-cnt = []
-for i in range(no):
- if sys.argv[i+1] in names:
-  #print(sys.argv[i+1])
-  cnt.append(d.loc[d.name==sys.argv[i+1]])
- else:
-  print('correct the name of ', sys.argv[i+1])
+url = "https://raw.githubusercontent.com/ayaka-wada/chansub/main/src/Number_of_channel_subscribers.csv"
+# print(url)
+# res = urllib.request.urlopen(url)
+# res = res.read().decode('utf-8"')
+csv = pd.read_csv(url, parse_dates=['date'], index_col='date')
+df = csv.interpolate(limit_direction='both')
 
-nme = []
-#print(len(cnt))
-for j in range(len(cnt)):
- for i in range(2000, 2022):
-  nme.append(int(cnt[j][i]))
-
-if len(cnt) == 1:
- plt.plot(x, nme, 'k-', label=sys.argv[1])
-
-if len(cnt) == 2:
- plt.plot(x, nme[0:21], 'k-', label=sys.argv[1])
- plt.plot(x, nme[21:42], 'k--', label=sys.argv[2])
-
-if len(cnt) == 3:
- plt.plot(x, nme[0:21], 'k-', label=sys.argv[1])
- plt.plot(x, nme[21:42], 'k--', label=sys.argv[2])
- plt.plot(x, nme[42:63], 'k:', label=sys.argv[3])
-
-if len(cnt) == 4:
- plt.plot(x, nme[0:21], 'k-', label=sys.argv[1])
- plt.plot(x, nme[21:42], 'k--', label=sys.argv[2])
- plt.plot(x, nme[42:63], 'k:', label=sys.argv[3])
- plt.plot(x, nme[63:84], 'k-.', label=sys.argv[4])
 
 def main():
- plt.legend()
- plt.savefig('result.png')
- plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(df.index, df['Tokino Sora'], label="Tokino Sora")
+    ax.plot(df.index, df['Houshyou Marine'], label="Houshyou Marine")
+    ax.plot(df.index, df['Shiranui Flare'], label="Shiranui Flare")
+    ax.plot(df.index, df['Usada Pekora'], label="Usada Pekora")
+    ax.plot(df.index, df['Shirogane Noel'], label="Shirogane Noel")
+    ax.plot(df.index, df['Kiryu Coco'], label="Kiryu Coco")
+
+
+    # ラベルの名前付け
+    ax.set_xlabel('Date')
+    ax.set_ylabel('channel subscribers')
+    # y軸の範囲設定
+    ax.set_ylim(0, 2300000)
+    # 指数表記から普通の表記に変換
+    plt.ticklabel_format(style='plain', axis='y')
+    # 凡例
+    plt.legend(loc="upper left", fontsize=10)
+    # x軸の文字を回転
+    plt.xticks(rotation=60)
+    plt.show()
+
